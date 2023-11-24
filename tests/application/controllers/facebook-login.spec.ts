6f5454ee122439aee970b966274291bfc5ccade2
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-11-24 12:18:38 -0300

    ensure 400 error when token is null or undefined

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -30,4 +30,22 @@ describe('FacebookLoginController', () => {
       data: new Error('The field token is required')
     })
   })
+
+  it('should return 400 if token is null', async () => {
+    const httpResponse = await sut.handle({ token: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field token is required')
+    })
+  })
+
+  it('should return 400 if token is undefined', async () => {
+    const httpResponse = await sut.handle({ token: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new Error('The field token is required')
+    })
+  })
 })
