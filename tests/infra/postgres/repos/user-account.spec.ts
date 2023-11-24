9919435fcb3ac905ac46f97a3e1c49024e56c289
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-11-24 00:12:45 -0300

    ensure user repo return user when called with valid email

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,58 @@
+import { LoadUserAccountRepository } from '@/data/contracts/repos'
+import { newDb } from 'pg-mem'
+import { Entity, PrimaryGeneratedColumn, Column, getRepository } from 'typeorm'
+
+class PgUserAccountRepository {
+  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ where: { email: params.email } })
+    if (pgUser != undefined) {
+      return {
+        id: pgUser.id.toString(),
+        name: pgUser?.name ?? undefined
+      }
+    }
+  }
+}
+
+@Entity({ name: 'usuarios' })
+class PgUser {
+  @PrimaryGeneratedColumn()
+    id!: number
+
+  @Column({ name: 'nome', nullable: true })
+    name!: string
+
+  @Column()
+    email!: string
+
+  @Column({ name: 'id_facebook', nullable: true })
+    facebookId!: string
+}
+
+describe('pgUserAccountRepository', () => {
+  let sut: PgUserAccountRepository
+  beforeAll(() => {
+
+  })
+  beforeEach(() => {
+
+  })
+  describe('load', () => {
+    it('should return an user when called with valid email', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      await connection.synchronize()
+      const pgUserRepo = connection.getRepository(PgUser)
+      const createdPgUser = pgUserRepo.create({ email: 'existing_email' })
+      await pgUserRepo.save(createdPgUser)
+      sut = new PgUserAccountRepository()
+      const account = await sut.load({ email: 'existing_email' })
+
+      expect(account).toEqual({ id: '1' })
+    })
+  })
+})
