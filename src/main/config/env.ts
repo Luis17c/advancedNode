7e470ce415ea5317c0decb18c22212fa69278dc8
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-11-27 22:21:49 -0300

    ensure facebookApi returns undefined when httpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '847045040762575',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? 'e4dd49ba911529530b75e35122011891'
+  }
+}
