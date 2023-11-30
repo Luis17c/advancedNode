439b8a10a1579a376bdc2561ec5e53ebfff3d76c
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-11-30 10:17:49 -0300

    move pg db config to ormconfig

--- src/domain/errors/server.ts
+++ src/domain/errors/server.ts
@@ -1,7 +1,6 @@
 export class ServerError extends Error {
   constructor (error?: Error) {
     super('Server Failed. Try again soon')
-    console.error(error)
     this.name = 'ServerError'
     this.stack = error?.stack
   }
--- src/main/index.ts
+++ src/main/index.ts
@@ -5,13 +5,10 @@ import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 
 import 'reflect-metadata'
-import { config } from '@/infra/postgres/helpers'
 import { createConnection } from 'typeorm'
 
-createConnection(config)
-  .then(() => app.listen(
-    env.appPort, () =>
-      console.log(`Server running at http://localhost:${env.appPort}`
-      )
+createConnection()
+  .then(() => app.listen(env.appPort, () =>
+    console.log(`Server running at http://localhost:${env.appPort}`)
   ))
   .catch(console.error)
