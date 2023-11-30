db166f93128a4a245194de56945e4b91ea253cc0
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-11-30 11:04:04 -0300

    express-router to functional function

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -1,10 +1,9 @@
 import { Controller } from '@/application/controllers'
 import { Request, RequestHandler, Response } from 'express'
 
-export const adaptExpressRoute = (controller: Controller): RequestHandler => {
-  return async (req: Request, res: Response) => {
-    const { statusCode, data } = await controller.handle({ ...req.body })
-    const json = statusCode == 200 ? data : { error: data.message }
-    res.status(statusCode).json(json)
-  }
+type Adapter = (controller: Controller) => RequestHandler
+export const adaptExpressRoute: Adapter = controller => async (req: Request, res: Response) => {
+  const { statusCode, data } = await controller.handle({ ...req.body })
+  const json = statusCode == 200 ? data : { error: data.message }
+  res.status(statusCode).json(json)
 }
