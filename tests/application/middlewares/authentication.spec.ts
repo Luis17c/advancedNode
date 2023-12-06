10667da767d56dbde3200067a9954905dbf4872c
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-12-06 11:20:07 -0300

    ensure authorizationMiddleware return if throws

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -15,7 +15,11 @@ class AuthenticationMiddleware {
   async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
     const error = new RequiredStringValidator(authorization, 'authorization').validate()
     if (error !== undefined) return forbidden()
-    await this.authorize({ token: authorization })
+    try {
+      await this.authorize({ token: authorization })
+    } catch {
+      return forbidden()
+    }
   }
 }
 
@@ -66,5 +70,16 @@ describe('AuthenticationMiddleware', () => {
       token: authorization
     })
     expect(authorize).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return 403 if authroize throws', async () => {
+    authorize.mockRejectedValueOnce(new Error('any_error'))
+
+    const httpResponse = await sut.handle({ authorization })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
   })
 })
