3b8be8e178f6459f927adda6c5fb6c7cee8b8e0f
Luís Cláudio Prado <luisclaudio.praado@gmail.com>
2023-12-06 15:06:18 -0300

    ensure expressMiddle respond with correct error and statusCode

--- tests/infra/http/express-middleware.spec.ts
+++ tests/infra/http/express-middleware.spec.ts
@@ -7,7 +7,8 @@ import { MockProxy, mock } from 'jest-mock-extended'
 type Adapter = (middleware: Middleware) => RequestHandler
 
 const adaptExpressMiddleware: Adapter = middleware => async (req, res, next) => {
-  await middleware.handle({ ...req.headers })
+  const { statusCode, data } = await middleware.handle({ ...req.headers })
+  res.status(statusCode).json(data)
 }
 
 interface Middleware {
@@ -25,8 +26,12 @@ describe('ExpressMiddleware', () => {
     req = getMockReq({ headers: { any: 'any' } })
     res = getMockRes().res
     next = getMockRes().next
+
     middleware = mock<Middleware>()
-    sut = adaptExpressMiddleware(middleware)
+    middleware.handle.mockResolvedValue({
+      statusCode: 500,
+      data: { error: 'any_error' }
+    })
   })
 
   beforeEach(() => {
@@ -41,15 +46,20 @@ describe('ExpressMiddleware', () => {
   })
 
   it('should call handle with empty request', async () => {
-    const req = getMockReq({ headers: { } })
-    const res = getMockRes().res
-    const next = getMockRes().next
-    const middleware = mock<Middleware>()
-    const sut = adaptExpressMiddleware(middleware)
+    req = getMockReq()
 
     await sut(req, res, next)
 
     expect(middleware.handle).toHaveBeenCalledWith({ })
     expect(middleware.handle).toHaveBeenCalledTimes(1)
+  })
+
+  it('should respond with correct error and statusCode', async () => {
+    await sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
